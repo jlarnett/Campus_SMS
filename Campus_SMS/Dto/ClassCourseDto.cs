e0b01521751ad2968de1e10b4edd043402baf87e
Johnny Arnett <im10g@hotmail.com>
2025-02-22 20:12:18 -0600

    Added functionality for mapping professors to classes. Some Controller + UI changes

---
+++ Campus_SMS/Dto/ClassCourseDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Campus_SMS.Entities.User;
+
+namespace Campus_SMS.Dto
+{
+    public class ClassCourseDto
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Description")]
+        [MaxLength(500)]
+        public string ClassDescription { get; set; }
+
+        [DisplayName("USI Class Identifier Code")]
+        [MaxLength(10)]
+        public string UsiClassIdentifier { get; set; }
+
+        public AppUserCheckboxViewModel[] AppUserIds { get; set; } = [];
+    }
+
+    public class AppUserCheckboxViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool IsChecked { get; set; }
+    }
+}
