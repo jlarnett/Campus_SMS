ad6130bb02840b6f5da4b9a669f04aa9f2c34262
Johnny Arnett <im10g@hotmail.com>
2025-03-22 19:06:30 -0500

    Populating Dashboard with some real data

--- Campus_SMS/Controllers/HomeController.cs
+++ Campus_SMS/Controllers/HomeController.cs
@@ -1,23 +1,46 @@
 using Campus_SMS.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Campus_SMS.Data;
+using Campus_SMS.Entities.User;
 using Microsoft.AspNetCore.Authorization;
-
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Campus_SMS.Views.Home.Vms;
 namespace Campus_SMS.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<AppUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, UserManager<AppUser> userManager)
         {
             _logger = logger;
+            _context = context;
+            _userManager = userManager;
         }
 
         [Authorize]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var courses = await _context.ClassProfessorMappings.Where(c => c.AppUserId.Equals(_userManager.GetUserId(User))).Include(c => c.Class).ToListAsync();
+            Dictionary<string, int> courseSmsCount = [];
+            Dictionary<string, int> courseEscalationCount = [];
+
+            foreach (var course in courses)
+            {
+                var count = await _context.SmsInteractions.Where(s => s.CourseId.Equals(course.ClassCourseId)).CountAsync();
+                var escalationCount =
+                    await _context.SmsInteractions.Where(s => s.CourseId.Equals(course.ClassCourseId) && s.IncomingSmsMessage.Contains("escalate")).CountAsync();
+                courseSmsCount.Add(course.Class.UsiClassIdentifier, count);
+                courseEscalationCount.Add(course.Class.UsiClassIdentifier, escalationCount);
+            }
+
+            DashboardVm vm = new DashboardVm(courseSmsCount, courseEscalationCount);
+            return View(vm);
         }
 
         [Authorize]
