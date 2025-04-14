7337533914b1a477fa052472bffab62f399c4847
Johnny Arnett <im10g@hotmail.com>
2025-04-14 18:46:41 -0500

    populated adminDashboardViewModel with data from database.

--- Campus_SMS/Controllers/AdminController.cs
+++ Campus_SMS/Controllers/AdminController.cs
@@ -1,21 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Campus_SMS.ViewModels;
 using Campus_SMS.Data; // for ApplicationDbContext
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Campus_SMS.Views.Admin.Vms;
 
 namespace Campus_SMS.Controllers
 {
-    public class AdminController(ApplicationDbContext context) : Controller
+    public class AdminController(ApplicationDbContext context, ILogger<AdminController> logger) : Controller
     {
         private readonly ApplicationDbContext _context = context;
+        private readonly ILogger<AdminController> _logger = logger;
 
-        public IActionResult Index(AdminDashboardViewModel adminDashboardViewModel)
+        public async Task<IActionResult> Index(AdminDashboardViewModel adminDashboardViewModel)
         {
-            ArgumentNullException.ThrowIfNull(adminDashboardViewModel);
-
             AdminDashboardViewModel model = new()
             {
-   
+                Classes = await _context.Courses.ToListAsync(),
+                Faculty = await _context.Users.ToListAsync(),
+                SmsInteractions = await _context.SmsInteractions.ToListAsync(),
+                Announcements = await _context.Announcements.ToListAsync(),
+                AIDocuments = await _context.OpenAIUploadedDocs.ToListAsync(),
+                SmsUsers = await _context.SmsUsers.ToListAsync(),
+                ClassProfessorMappings = await _context.ClassProfessorMappings.ToListAsync()
             };
 
             return View(model);
